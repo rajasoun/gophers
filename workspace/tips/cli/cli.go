8a5befe642aad3e520f87b806e87954654d45d9a
bandilbulbul <bulbul.bandil@srsconsultinginc.com>
2021-06-02 14:28:06 +0000

    added more testcases

--- workspace/tips/cli/cli.go
+++ workspace/tips/cli/cli.go
@@ -1,8 +1,19 @@
 package cli
 
+//return title: userInput
 func GetTopic() string {
 	// pass:1 - Make test pass
 	// hardcoded
 	//:ToDo: Get User Input - Mock GetInput in Test
-	return "git status"
+
+	title := scanTitleFromConsole() //title
+
+	return title[0]
+
+}
+func scanTitleFromConsole() []string {
+	//var title string
+	//fmt.Scanf("%s", &title)
+	title := []string{"git status", "Everyday Git in twenty commands or so", "Show helpful guides that come with Git", "Delete remote branch", "Saving current state of tracked files without commiting"}
+	return title
 }
--- workspace/tips/model/tips.go
+++ workspace/tips/model/tips.go
@@ -1,18 +1,32 @@
 package model
 
 import (
-	"strings"
+	"encoding/json"
 )
 
-func GetTip(topic string) string {
+type Tips struct {
+	Title string `json:"title"`
+	Tip   string `json:"tip"`
+}
+
+//return tip
+func GetTip(title string) string {
 	// pass:1 - Make test pass
 	// hardcoded
 	//:ToDo: Reafactor To Load Tips from JSON
-	var result string
-	if strings.Compare(topic, "git status") == 0 {
-		result = "git status -s"
-	} else {
-		result = "Tips Not Available for Topic"
+	data := loadTipsFromJson()
+	for index, _ := range data {
+		//if strings.Compare(title, data[index].Title) == 0 {
+		if title == data[index].Title {
+			return data[index].Tip
+		}
 	}
+	return "Tips Not Available for Topic"
+}
+
+func loadTipsFromJson() []Tips {
+	data := `[{"title":"git status","tip":"git status -s"},{"title": "Everyday Git in twenty commands or so","tip": "git help everyday"},{"title": "Show helpful guides that come with Git","tip": "git help -g"},{"title": "Delete remote branch","tip": "git push origin --delete <remote_branchname>"},{"title": "Saving current state of tracked files without commiting","tip": "git stash"}]`
+	var result []Tips
+	json.Unmarshal([]byte(data), &result)
 	return result
 }
