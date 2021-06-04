b3cb9c178718dd493c1f92011f67e56eca939615
rajasoun <rajasoun@cisco.com>
2021-06-04 12:08:09 +0000

    dev container TDD

--- ci-shell/lib/validators.sh
+++ ci-shell/lib/validators.sh
@@ -23,12 +23,34 @@ function check() {
     fi
 }
 
+function check_detail() {
+    LABEL=$1
+    shift
+    echo -e "\n🧪 Testing $LABEL"
+    if "$@"; then
+        echo "✅  Passed!"
+        return 0
+    else
+        echo_std_err "❌ $LABEL check Failed!"
+        return 1
+    fi
+}
+
+function check_system_details() {
+    check_detail "non-root-user" id "${USER}"
+    check_detail "locale" [ "$(locale -a | grep en_US.utf8)" ]
+    check_detail "sudo" sudo echo "sudo works."
+    check_detail "zsh" zsh --version
+    check_detail "oh-my-zsh" [ -d "$HOME/.oh-my-zsh" ]
+    check_detail "login-shell-path" [ -f "/etc/profile.d/00-restore-env.sh" ]
+    check_detail "code" which code
+}
+
 function report_results() {
     FAILED=("$@")
     echo "${FAILED[@]}"
     if [ ${#FAILED[@]} -ne 0 ]; then
         echo_std_err "\n💥  Failed tests:" "${#FAILED[@]}"
-        # echo_std_err "\nOverall Status: Failed! ❌" 
         return 1
     else
         echo -e "\n💯  All Passed!"
@@ -36,14 +58,73 @@ function report_results() {
     fi
 }
 
-function check_common_os_packages(){
+function check_dev_packages(){
    local FAILED=()
-   pkgs=(git shellspec kcov jq nc gh curl)
+   pkgs=(git go shellspec kcov jq nc gh curl python3)
    for pkg in "${pkgs[@]}"
    do 
         check "$pkg" || FAILED+=("$pkg")
    done
    report_results "${FAILED[@]}"
 }
+
+function check_toolz_packages(){
+   local FAILED=()
+   pkgs=(husky commitlint commitizen shellcheck)
+   for pkg in "${pkgs[@]}"
+   do 
+        check "$pkg" || FAILED+=("$pkg")
+   done
+   report_results "${FAILED[@]}"
+}
+
+# Check common os packages are installed
+# 
+function _common_packages() {
+    local FAILED=()
+    LABEL=$1
+    shift
+    echo -e "\n🧪 Testing $LABEL"
+    if dpkg-query --show -f='${Package}: ${Version}\n' "$@"; then
+        echo "💯  All Passed! ✅"
+        return 0
+    else
+        echo_std_err "$LABEL check Failed! ❌"
+        FAILED+=("$LABEL")
+        return 1
+    fi
+}
+
+function check_common_packages(){
+    PACKAGE_LIST="apt-utils \
+        git \
+        openssh-client \
+        less \
+        iproute2 \
+        procps \
+        curl \
+        wget \
+        unzip \
+        nano \
+        jq \
+        lsb-release \
+        ca-certificates \
+        apt-transport-https \
+        dialog \
+        gnupg2 \
+        libc6 \
+        libgcc1 \
+        libgssapi-krb5-2 \
+        liblttng-ust0 \
+        libstdc++6 \
+        zlib1g \
+        locales \
+        sudo"
+    echo -e "\n🧪 Testing ${PACKAGE_LIST}"
+    _common_packages "common-os-packages" ${PACKAGE_LIST}
+}
+
+
+
 
 
--- ci-shell/spec/ci_shell_spec.sh
+++ ci-shell/spec/ci_shell_spec.sh
@@ -2,10 +2,22 @@
 
 Describe "System Test : "
     Include lib/validators.sh
-    Context "ci-shell - common-os-packages : "
-        It "common-os-packages"
-            When call check_common_os_packages 
+    Context "ci-shell - check packages : "
+        It "check_common_packages"
+            When call check_common_packages 
             The output should include "All Passed!"
+        End
+        It "check_dev_packages"
+            When call check_dev_packages 
+            The output should include "All Passed!"
+        End
+        It "check_toolz_packages"
+            When call check_toolz_packages 
+            The output should include "All Passed!"
+        End
+        It "check_system_details"
+            When call check_system_details 
+            The output should include "Passed!"
         End
     End
     Context "ci-shell - user-management : "
@@ -26,6 +38,23 @@ Describe "Unit Test : validators.sh"
         When call check uname
         The output should include "Passed!"
     End
+    It "check_deatil function with "zsh" zsh --version"
+        When call check_detail "zsh" zsh --version
+        The output should include "Passed!"
+    End
+    It "check_deatil function with zsh --version1 should fail"
+        When call check_detail "zsh" zsh --version1
+        The status should be failure
+        The output should include "Testing zsh"
+        The error should include "Failed!"
+    End
+    It "_common_packages function with invalid packages should fail"
+        PACKAGE_LIST="dummy"
+        When call _common_packages "common-os-packages" ${PACKAGE_LIST}
+        The status should be failure
+        The output should include "Testing"
+        The error should include "Failed!"
+    End 
     It "check function with dummy should fail"
         When call check dummy
         The status should be failure
