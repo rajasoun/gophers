e64df0e0e03bae59023d7916bd19959860889c28
bandilbulbul <bulbul.bandil@srsconsultinginc.com>
2021-06-21 08:14:41 +0000

    added unittest in controller

---
+++ docs/notes/DataStructure.md
@@ -0,0 +1,6 @@
+# Data Structure and Types in Go
+1. Array
+1. Slice
+1. Struct
+1. Interfaces
+1. Maps
--- workspace/tips/controller/tips_test.go
+++ workspace/tips/controller/tips_test.go
@@ -10,10 +10,17 @@ import (
 type mock struct {
 	title string
 }
+type helpCommand struct {
+	command string
+}
 
 func (m mock) scanTitleFromConsole() string {
 	m.title = "delete"
 	return m.title
+}
+func (hc helpCommand) scanTitleFromConsole() string {
+	hc.command = "git-tip --all"
+	return hc.command
 }
 
 func TestGetTipForTopic(t *testing.T) {
@@ -32,6 +39,15 @@ func TestGetTipForTopic(t *testing.T) {
 		GetTipForTopic(&buffer, sn)
 		got := buffer.String()
 		want := "Default tip: \n \"Saving current state of tracked"
+		assert.Contains(t, got, want)
+	})
+	t.Run("git-tip --all", func(t *testing.T) {
+		hc := &helpCommand{}
+		buffer := bytes.Buffer{}
+		GetTipForTopic(&buffer, hc)
+		got := buffer.String()
+		//want := 166
+		want := "Everyday Git in twenty commands or so"
 		assert.Contains(t, got, want)
 	})
 }
