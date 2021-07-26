bc1b6575f8fea017fd42df0457b0850b0619d32d
bandilbulbul <bulbul.bandil@srsconsultinginc.com>
2021-07-26 12:23:39 +0000

    added viber lib for reading file

--- workspace/rest-client/client/client.go
+++ workspace/rest-client/client/client.go
@@ -1,74 +1,53 @@
 package client
 
 import (
-	"bufio"
 	"context"
+	"fmt"
 	"net/http"
-	"os"
-	"strings"
 
 	"github.com/cristalhq/oauth2"
+	"github.com/spf13/viper"
 )
 
-var values = make(map[string]string)
-var openFile = os.Open
+type configuration struct {
+	ClientID     string `mapstructure:"client_id"`
+	ClientSecret string `mapstructure:"client_secret"`
+	UserID       string `mapstructure:"user_name"`
+	UserPwd      string `mapstructure:"password"`
+	TokenURL     string `mapstructure:"token_url"`
+	ProductURL   string `mapstructure:"products_url"`
+}
 
-func loadDatafromEnv() (map[string]string, error) {
-	file, err := openFile("../data.env")
+var configFilepath string = "../configfile" //current working directory
+
+func LoadfromEnv(path string) (configuration, error) {
+	var config_data configuration
+	viper.AddConfigPath(path)
+	viper.SetConfigName("config")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
 	if err != nil {
-		//	fmt.Printf("error opening file: %v\n", err)
-		return map[string]string{}, err
+		return configuration{}, err
 	}
-	reader := bufio.NewReader(file)
-	data, err := readln(reader)
-	for err == nil {
-		data, err = readln(reader)
-	}
-	return data, nil
+	viper.Unmarshal(&config_data)
+	fmt.Println(config_data)
+	return config_data, nil
+
 }
 
-func readln(reader *bufio.Reader) (map[string]string, error) {
-	var (
-		isPrefix bool  = true
-		err      error = nil
-		line     []byte
-	)
-	for isPrefix && err == nil {
-		line, isPrefix, err = reader.ReadLine()
-		lineBy := string(line)
-		if strings.Contains(lineBy, "=") {
-			res1 := strings.Index(lineBy, "=")
-			values[lineBy[0:(res1)]] = lineBy[res1+1:]
-		}
-	}
-	return values, err
-}
-func findMapValues(oauthId string) string {
-	clientValues, _ := loadDatafromEnv()
-	for Key, value := range clientValues {
-		if strings.Contains(Key, oauthId) {
-			return value
-		}
-	}
-	return "not available"
-}
-
-var userName string = findMapValues("user.name")
-var pass string = findMapValues("password")
-
-func getAccessToken() (string, string, error) {
-	config := oauth2.Config{
-		ClientID:     findMapValues("token.client.id"),
-		ClientSecret: findMapValues("token.client.secret"),
-		TokenURL:     findMapValues("token.request.url"),
+func getToken() (*oauth2.Token, error) {
+	configData, _ := LoadfromEnv(configFilepath)
+	oauthConfig := oauth2.Config{
+		ClientID:     configData.ClientID,
+		ClientSecret: configData.ClientSecret,
+		TokenURL:     configData.TokenURL,
 	}
 	// create a client
-	new_client := oauth2.NewClient(http.DefaultClient, config)
-	token, err := new_client.CredentialsToken(context.Background(), userName, pass)
+	new_client := oauth2.NewClient(http.DefaultClient, oauthConfig)
+	token, err := new_client.CredentialsToken(context.Background(), configData.UserID, configData.UserPwd)
 	if err != nil {
-		//fmt.Println("Erorr from main:", err)
-		return "", "", err
+		return nil, err
 	}
-	return token.AccessToken, token.TokenType, nil
-
+	return token, nil
 }
--- workspace/rest-client/client/client_test.go
+++ workspace/rest-client/client/client_test.go
@@ -1,160 +1,29 @@
 package client
 
 import (
-	"errors"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadDatafromEnv(t *testing.T) {
-	got, _ := loadDatafromEnv()
-	want := 8
-	assert.Equal(t, len(got), want)
-
-}
-func TestGetAccesToken(t *testing.T) {
-	t.Run("Checking access token , token string length should not be 0 ", func(t *testing.T) {
-		got_token, _, _ := getAccessToken()
-		expectedToken := ""
-		assert.NotEqual(t, len(got_token), len(expectedToken))
-	})
-	t.Run("Checking token type", func(t *testing.T) {
-		_, got_tokenType, _ := getAccessToken()
-		expectedTokenType := "Bearer"
-		assert.Equal(t, got_tokenType, expectedTokenType)
-
-	})
-}
-
-func TestGetError(t *testing.T) {
-	t.Run("Checking Error if username and password are wrong", func(t *testing.T) {
-		userName = "user1"
-		pass = "pss12"
-		_, _, err := getAccessToken()
-		assert.Error(t, err)
-	})
-}
-
-func TestGettingErrorOnReadFile(t *testing.T) {
-	t.Run("Loading invalid env File should fail", func(t *testing.T) {
-		// Mocked function for os.ReadFile
-		file_read := func(string) (*os.File, error) {
-			myErr := errors.New("error")
-			return nil, myErr
+func Test_LoadfromEnv(t *testing.T) {
+	t.Run("Checking error", func(t *testing.T) {
+		_, err := LoadfromEnv("/data")
+		if err != nil {
+			assert.Error(t, err)
 		}
-		openFile = file_read
-		_, err := loadDatafromEnv()
-		assert.Error(t, err)
+	})
+	t.Run("Checking loaded data", func(t *testing.T) {
+		got, _ := LoadfromEnv("../configfile")
+		//want := configuration{}
+		assert.NotNil(t, got)
+
 	})
 
 }
-
-func TestFindMapValues(t *testing.T) {
-	t.Run("getting invalid data if key is not present", func(t *testing.T) {
-		got := findMapValues("dummy")
-		want := "not available"
-		assert.Equal(t, got, want)
-
+func Test_GetToken(t *testing.T) {
+	t.Run("Checking token type", func(t *testing.T) {
+		got, _ := getToken()
+		assert.NotNil(t, got)
 	})
 }
-
-// func newServer(h func(w http.ResponseWriter, r *http.Request)) *httptest.Server {
-// 	return httptest.NewServer(http.HandlerFunc(h))
-// }
-
-// func TestPasswordCredentialsTokenRequest(t *testing.T) {
-// 	ts := newServer(func(w http.ResponseWriter, r *http.Request) {
-// 		defer r.Body.Close()
-// 		expected := "/token"
-// 		if r.URL.String() != expected {
-// 			t.Errorf("URL = %q; want %q", r.URL, expected)
-// 		}
-
-// 		headerAuth := r.Header.Get("Authorization")
-// 		expected = "Basic Q0xJRU5UX0lEOkNMSUVOVF9TRUNSRVQ="
-// 		if headerAuth != expected {
-// 			t.Errorf("Authorization header = %q; want %q", headerAuth, expected)
-// 		}
-
-// 		headerContentType := r.Header.Get("Content-Type")
-// 		expected = "application/x-www-form-urlencoded"
-// 		if headerContentType != expected {
-// 			t.Errorf("Content-Type header = %q; want %q", headerContentType, expected)
-// 		}
-
-// 		body, err := ioutil.ReadAll(r.Body)
-// 		if err != nil {
-// 			t.Errorf("Failed reading request body: %s.", err)
-// 		}
-
-// 		expected = "grant_type=password&password=password1&scope=scope1+scope2&username=user1"
-// 		if string(body) != expected {
-// 			t.Errorf("res.Body = %q; want %q", string(body), expected)
-// 		}
-
-// 		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
-// 		_, _ = w.Write([]byte("access_token=ProperToken&scope=user&token_type=bearer"))
-// 	})
-// 	defer ts.Close()
-
-// 	//client := newClient(ts.URL)
-// 	// tok, err := client.CredentialsToken(context.Background(), "user1", "password1")
-// 	// if err != nil {
-// 	// 	t.Error(err)
-// 	// }
-// 	User = "user1"
-// 	Pass = "password1"
-// 	got1, _ := setonOAuthConfig()
-// 	fmt.Print(got1)
-
-// 	// if !tok.Valid() {
-// 	// 	t.Fatalf("Token invalid. Got: %#v", tok)
-// 	// }
-
-// 	expected := "ProperToken"
-// 	if got1 != expected {
-// 		t.Errorf("AccessToken = %q; want %q", got1, expected)
-// 	}
-
-// 	// expected = "bearer"
-// 	// if tok.TokenType != expected {
-// 	// 	t.Errorf("TokenType = %q; want %q", tok.TokenType, expected)
-// 	// }
-// }
-
-// type Config struct {
-// 	ClientID     string // ClientID is the application's ID.
-// 	ClientSecret string // ClientSecret is the application's secret.
-// 	AuthURL      string // AuthURL is a URL for authentication.
-// 	TokenURL     string // TokenURL is a URL for retrieving a token.
-// 	//Mode         Mode     // Mode represents how tokens are represented in requests.
-// 	RedirectURL string   // RedirectURL is the URL to redirect users going through the OAuth flow.
-// 	Scopes      []string // Scope specifies optional requested permissions.
-// }
-// type Client struct {
-// 	client *http.Client
-// 	config Config
-// }
-
-// func NewClient(client *http.Client, config Config) *Client {
-// 	c := &Client{
-// 		client: client,
-// 		config: config,
-// 	}
-// 	return c
-// }
-
-// func newClient(url string) *Client {
-// 	cfg := Config{
-// 		ClientID:     "CLIENT_ID",
-// 		ClientSecret: "CLIENT_SECRET",
-// 		AuthURL:      url + "/auth",
-// 		TokenURL:     url + "/token",
-// 		//Mode:         AutoDetectMode,
-// 		RedirectURL: "REDIRECT_URL",
-// 		Scopes:      []string{"scope1", "scope2"},
-// 	}
-// 	return NewClient(http.DefaultClient, cfg)
-// }
