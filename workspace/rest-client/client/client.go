fdf16b0084bc02640616f001d894e0d96758b49b
bandilbulbul <bulbul.bandil@srsconsultinginc.com>
2021-07-30 13:28:20 +0000

    added test cases on error

--- workspace/rest-client/client/client.go
+++ workspace/rest-client/client/client.go
@@ -3,7 +3,7 @@ package client
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 
@@ -11,6 +11,16 @@ import (
 
 	"github.com/cristalhq/oauth2"
 )
+
+var (
+	client      HTTPClient
+	httpRequest = http.NewRequest
+	read_All    = io.ReadAll
+)
+
+func init() {
+	client = &http.Client{}
+}
 
 type configuration struct {
 	ClientID     string `mapstructure:"client_id"`
@@ -65,15 +75,10 @@ type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
-var client HTTPClient
-
-func init() {
-	client = &http.Client{}
-}
 func getHttpRequest(header http.Header, url string) (*http.Response, error) {
-	request, err := http.NewRequest("GET", url, nil)
+	request, err := httpRequest("GET", url, nil)
+
 	if err != nil {
-		fmt.Print(err)
 		return nil, err
 	}
 	request.Header = header
@@ -89,26 +94,29 @@ type data struct {
 }
 
 func getDatafromRestapi(response *http.Response) ([]data, string, error) {
-	bodyBytes, err := ioutil.ReadAll(response.Body)
+	bodyBytes, err := read_All(response.Body)
 	if err != nil {
 		return nil, "invalid data", err
 	}
 	var dataString []data
+
 	json.Unmarshal(bodyBytes, &dataString)
 	return dataString, string(bodyBytes), nil
 }
 
 func writeDataintoJson(jsonData []data, fileSavePath string) error {
-	json_Data, err := json.MarshalIndent(jsonData, "", "")
-	if err != nil {
-		return err
-	}
+	json_Data, _ := json.MarshalIndent(jsonData, "", "")
 	//var fileSavePath ="configfile/configJsonData.json"
-	err = ioutil.WriteFile(fileSavePath, json_Data, 0644)
+	err := ioutil.WriteFile(fileSavePath, json_Data, 0644)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func writeintoDatabase() error {
+	return nil
+
 }
 func Run() {
 
--- workspace/rest-client/client/client_test.go
+++ workspace/rest-client/client/client_test.go
@@ -2,7 +2,8 @@ package client
 
 import (
 	"bytes"
-	"fmt"
+	"errors"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -58,7 +59,7 @@ func Test_GetToken(t *testing.T) {
 
 func Test_SetHeader(t *testing.T) {
 	//to do mock the header to check func.
-	t.Run("", func(t *testing.T) {
+	t.Run("Checking Request Header on passing token data", func(t *testing.T) {
 		configData, _ := loadfromEnv("../configfile")
 		token, err := getToken(configData)
 		if err != nil {
@@ -81,16 +82,16 @@ func (m *mockClient) Do(req *http.Request) (*http.Response, error) {
 	return GetDoFunc(req)
 }
 func Test_GetHttpRequest(t *testing.T) {
-	json := `{"name":"Test Name","full_name":"test full name","owner":{"login": "octocat"}}`
-	// create a new reader with that JSON
-	reader := ioutil.NopCloser(bytes.NewReader([]byte(json)))
-	GetDoFunc = func(*http.Request) (*http.Response, error) {
-		return &http.Response{
-			StatusCode: 200,
-			Body:       reader,
-		}, nil
-	}
 	t.Run("Checking fetch valid data from restapi", func(t *testing.T) {
+		json := `{"name":"Test Name","full_name":"test full name","owner":{"login": "octocat"}}`
+		// create a new reader with that JSON
+		reader := ioutil.NopCloser(bytes.NewReader([]byte(json)))
+		GetDoFunc = func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       reader,
+			}, nil
+		}
 		client = &mockClient{}
 		resp, err := getHttpRequest(nil, "@com.in")
 		bodyBytes, err := ioutil.ReadAll(resp.Body)
@@ -128,20 +129,71 @@ func Test_getDatafromRestapi(t *testing.T) {
 		assert.EqualValues(t, 200, resp.StatusCode)
 		assert.Contains(t, got, "hasUnlimitedLicenses")
 	})
+	t.Run("Checking error on passing invalid response ", func(t *testing.T) {
+		inputs := `{"hasUnlimitedLicenses":false,"lastModifiedBy":"test full name","createdBy":"login"}`
+		// create a new reader with that JSON
+		readerInputs := ioutil.NopCloser(bytes.NewReader([]byte(inputs)))
+		GetDoFunc = func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       readerInputs,
+			}, nil
+		}
+		client = &mockClient{}
+		resp, err := getHttpRequest(nil, "@com.in")
+		if err != nil {
+			t.Fatal(err)
+		}
+		// Mocked function for os.ReadFile
+		file_read := func(io.Reader) ([]byte, error) {
+			myErr := errors.New("Simulated error")
+			return nil, myErr
+		}
+		read_All = file_read
+		//assert.Error(t, err)
+		_, got, err := getDatafromRestapi(resp)
+		assert.Error(t, err)
+		assert.EqualValues(t, got, "invalid data")
+	})
 
 }
 
 func Test_WriteDataintoJson(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+	t.Run("checking writing data to json file", func(t *testing.T) {
 		test_data := []data{
 			{HasUnlimited: false},
 			{HasUnlimited: true},
 		}
-		var fileRead = os.ReadFile
+		var fileRead = ioutil.ReadFile
 		err := writeDataintoJson(test_data, "test.json")
 		got, _ := fileRead("test.json")
-		fmt.Print(string(got))
 		assert.Contains(t, string(got), "hasUnlimitedLicenses")
 		assert.NoError(t, err)
 	})
+	t.Run("Checking error on passing invalid file path", func(t *testing.T) {
+		test_data := []data{
+			{HasUnlimited: true},
+		}
+		err := writeDataintoJson(test_data, "../test/client.json")
+		assert.Error(t, err)
+
+	})
+}
+
+func Test_WriteintoDatabase(t *testing.T) {
+	got := writeintoDatabase()
+	assert.NoError(t, got)
+}
+
+func Test_GetHttpRequestWithError(t *testing.T) {
+	t.Run("Checking error on passing invalid url", func(t *testing.T) {
+		file_read := func(string, string, io.Reader) (*http.Request, error) {
+			myErr := errors.New("Request error")
+			return nil, myErr
+		}
+		httpRequest = file_read
+		_, err := getHttpRequest(nil, "dummyURL@dummy.com.in")
+		assert.Error(t, err)
+	})
+
 }
